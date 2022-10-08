7c40ee0e756370a24701f3433e05ceb12d1c267f
frankopkusianwar <okirorfrank3@gmail.com>
2022-10-08 22:46:24 +0300

    Add nodemon

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 import {app} from './index.js'
 
 const PORT = 5000
-app.listen(PORT, () => console.log(`application Running on port ${PORT}`))
+app.listen(PORT, () => console.log(`application running on port ${PORT}`))
